b45920dbdbca8bcbf472a49e368a83f569a408bc
AliKemalMiloglu <150537208+AliKemalMiloglu@users.noreply.github.com>
2025-04-01 23:04:17 +0300

    Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,38 @@
 import java.util.Random;
 
 public class Player {
-	private Tent tent;
-	private TreasureBox treasureBox;
-	private HazardBox hazardBox;
-	private String name;
-	private Random rand;
+    private Tent tent;
+    private TreasureBox treasureBox;
+    private HazardBox hazardBox;
+    private String name;
+    private Random rand;
 
-	public Player(String initialName) {
-		this.tent = new Tent();
-		this.treasureBox = new TreasureBox();
-		this.hazardBox = new HazardBox();
-		this.name = initialName;
-		this.rand = new Random();
-	}
+    public Player(String initialName) {
+        this.tent = new Tent();
+        this.treasureBox = new TreasureBox();
+        this.hazardBox = new HazardBox();
+        this.name = initialName;
+        this.rand = new Random();
+    }
 
-	public int rollDice() {
-		int randomIndex = rand.nextInt(30);
-		return randomIndex;
-	}
+    public int rollDice() {
+        int randomIndex = rand.nextInt(30) + 1;
+        return randomIndex;
+    }
 
-	public int calculateScore() {
+    public int calculateScore() {
+        Gold gold =new Gold();
+        Obsidian obsidian =new Obsidian();
+        Turquoise turquoise =new Turquoise();
+        int pointGold=tent.getFrequencyOf(gold)*10;
+        int pointObsidian=tent.getFrequencyOf(obsidian)*5;
+        int pointTurquoise=tent.getFrequencyOf(turquoise);
+        int totalpoint = pointGold + pointObsidian + pointTurquoise;
+        return  totalpoint;
+    }
 
-	}
-
-	public String toString() {
-
-	}
+    public String toString() {
+        return "The username of the player: "+this.name+" and the total point of this player is " + calculateScore();
+    }
 
 }
